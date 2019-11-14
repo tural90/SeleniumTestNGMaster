8b87c1724d23d121179bd82bf9dd9f7b1809cc94
CybertekSchool <github@cybertekschool.com>
2019-11-14 10:57:34 -0500

    checkbox example

---
+++ src/test/java/com/cybertek/tests/o1_intellij_shortcuts/RadioButtonsTest.java
@@ -0,0 +1,42 @@
+package com.cybertek.tests.o1_intellij_shortcuts;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class RadioButtonsTest {
+
+    @Test
+    public void test1(){
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+        driver.get("http://practice.cybertekschool.com/radio_buttons");
+
+        WebElement blue = driver.findElement(By.id("blue"));
+        WebElement red = driver.findElement(By.id("red"));
+
+        System.out.println("is blue selected: "+  blue.isSelected());
+        System.out.println("is red selected: "+  red.isSelected());
+
+        // verify if blue is selected
+        Assert.assertTrue(blue.isSelected());
+        // verify red is not selected
+        Assert.assertFalse(red.isSelected());
+
+        System.out.println("Clicking on red");
+
+        red.click();
+        System.out.println("is blue selected: "+  blue.isSelected());
+        System.out.println("is red selected: "+  red.isSelected());
+
+        // verify blue is NOT selected
+        Assert.assertFalse(blue.isSelected());
+        // verify red is selected
+        Assert.assertTrue(red.isSelected());
+
+    }
+}
