1aac035789249980d07d1dda20524b2389116d58
CybertekSchool <github@cybertekschool.com>
2019-11-15 15:51:34 -0500

    list

--- src/test/java/com/cybertek/tests/d8_select_list/SelectClassTest.java
+++ src/test/java/com/cybertek/tests/d8_select_list/SelectClassTest.java
@@ -1,4 +1,32 @@
 package com.cybertek.tests.d8_select_list;
 
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
 public class SelectClassTest {
+
+    @Test
+    public void printAllOptions(){
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/dropdown");
+
+        // Locate the element with the select tag
+        WebElement element = driver.findElement(By.id("a"));
+
+        // Create Select object by passing the element from step 2 as constructor
+        Select stateList = new Select(element);
+
+        // getOptions() --> returns all the available options from the dropdown list
+        List<WebElement> options = stateList.getOptions();
+
+        for (WebElement option : options) {
+            System.out.println(option.getText());
+        }
+    }
 }
