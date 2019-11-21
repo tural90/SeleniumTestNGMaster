f71856c37e8ab15c6f62e7e664b673b12611edcf
CybertekSchool <github@cybertekschool.com>
2019-11-21 13:01:31 -0500

    javascrip executor

--- src/test/java/com/cybertek/tests/d11_file_upload_actions_class/ActionsExamplesTest.java
+++ src/test/java/com/cybertek/tests/d11_file_upload_actions_class/ActionsExamplesTest.java
@@ -40,10 +40,42 @@ public class ActionsExamplesTest {
         Actions actions = new Actions(driver);
         Thread.sleep(2000);
         // moveToElement() --> moves the mouse on top of the given element
-        // perform() --> complete the action
+        // perform() --> complete the action, using every time after calling action
         actions.moveToElement(img1).perform();
 
         Assert.assertTrue(driver.findElement(By.linkText("View profile")).isDisplayed());
+
+    }
+
+    @Test
+    public void dragdroptest() throws InterruptedException {
+        driver.get("https://demos.telerik.com/kendo-ui/dragdrop/index");
+        // source --> we will drag this element
+        WebElement source = driver.findElement(By.id("draggable"));
+        // target --> we drop here
+        WebElement target = driver.findElement(By.id("droptarget"));
+
+        Actions actions = new Actions(driver);
+
+        Thread.sleep(2000);
+        // dragAndDrop --> drags the source to target
+        actions.dragAndDrop(source, target).perform();
+
+    }
+
+    @Test
+    public void dragdropbychainingtest(){
+        driver.get("https://demos.telerik.com/kendo-ui/dragdrop/index");
+        // source --> we will drag this element
+        WebElement source = driver.findElement(By.id("draggable"));
+        // target --> we drop here
+        WebElement target = driver.findElement(By.id("droptarget"));
+
+        Actions actions = new Actions(driver);
+    //  we can chain multiple actions
+        // at the end we call the build().perform()
+        // build --> is used to chain the actions.
+        actions.moveToElement(source).clickAndHold().moveToElement(target).release().build().perform();
 
     }
 }
---
+++ src/test/java/com/cybertek/tests/d11_file_upload_actions_class/JavaScriptExecutorTest.java
@@ -0,0 +1,69 @@
+package com.cybertek.tests.d11_file_upload_actions_class;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import javax.transaction.TransactionRequiredException;
+
+public class JavaScriptExecutorTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod() {
+        driver = WebDriverFactory.getDriver("chrome");
+    }
+
+    @AfterMethod
+    public void teardownMethod() throws InterruptedException {
+        Thread.sleep(20000);
+        driver.quit();
+    }
+
+    @Test
+    public void click(){
+        driver.get("http://practice.cybertekschool.com/");
+        WebElement element = driver.findElement(By.linkText("Home"));
+       // this how the JavascriptExecutor object is created
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        // executeScript --> method used to pass js command
+        // first string argiment is the javascript code
+        // second argument is the webelement on which the action will be take
+        jse.executeScript("arguments[0].click();", element);
+
+    }
+
+    @Test
+    public void type() throws InterruptedException {
+        driver.get("http://practice.cybertekschool.com/dynamic_controls");
+        WebElement element = driver.findElement(By.cssSelector("input[disabled]"));
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        String str = "hello";
+
+//        element.sendKeys(str);
+        Thread.sleep(2000);
+        jse.executeScript("arguments[0].setAttribute('value', '" + str +"')", element);
+
+    }
+
+    @Test
+    public void scroll() throws InterruptedException {
+        driver.get("http://practice.cybertekschool.com/infinite_scroll");
+
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+
+        for (int i = 0; i < 5; i++) {
+            jse.executeScript("scroll(0, 500);");
+            Thread.sleep(2000);
+
+        }
+
+    }
+
+
+}
