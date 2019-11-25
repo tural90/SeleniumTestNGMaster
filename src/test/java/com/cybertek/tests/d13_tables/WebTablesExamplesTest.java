399213ef7fb5821b782ada33bff151f5547f5350
CybertekSchool <github@cybertekschool.com>
2019-11-25 15:54:40 -0500

    tables part 2

--- src/test/java/com/cybertek/tests/d13_tables/WebTablesExamplesTest.java
+++ src/test/java/com/cybertek/tests/d13_tables/WebTablesExamplesTest.java
@@ -28,7 +28,7 @@ public class WebTablesExamplesTest {
     }
 
     @Test
-    public void printTable(){
+    public void printTable() {
         WebElement myTable = driver.findElement(By.id("table1"));
         System.out.println(myTable.getText());
 
@@ -37,7 +37,7 @@ public class WebTablesExamplesTest {
     }
 
     @Test
-    public void getAllHeaders(){
+    public void getAllHeaders() {
         // get all column names individually
         List<WebElement> headers = driver.findElements(By.xpath("//table[@id='table1']//th"));
         System.out.println("headers.size() = " + headers.size());
@@ -53,7 +53,7 @@ public class WebTablesExamplesTest {
     }
 
     @Test
-    public void printTableSize(){
+    public void printTableSize() {
         // NUMBER OF COLUMNS
         List<WebElement> headers = driver.findElements(By.xpath("//table[@id='table1']//th"));
         System.out.println("number of columns: = " + headers.size());
@@ -64,12 +64,71 @@ public class WebTablesExamplesTest {
         // NUMBER OF ROWS WITHOUT HEADERS
         List<WebElement> allRowsExcludingHeaders = driver.findElements(By.xpath("//table[@id='table1']/tbody/tr"));
         System.out.println("number of rows excluding headers: " + allRowsExcludingHeaders.size());
+        // '''
 
     }
 
 
+    @Test
+    public void getRow() {
+        // get any from the table using index. we will ignore the headers. 1st row is the 1st row in the body
+        WebElement row = driver.findElement(By.xpath("//table[@id='table1']/tbody/tr[1]"));
+        System.out.println(row.getText());
+
+        // get all the table row dynamically. we can do it in 2 steps
+        // 1. get the number rows
+        // based on the number of rows iterate each row
+
+        List<WebElement> allRowsExcludingHeaders = driver.findElements(By.xpath("//table[@id='table1']/tbody/tr"));
+        System.out.println("number of rows excluding headers: " + allRowsExcludingHeaders.size());
+
+        for (int i = 1; i <= allRowsExcludingHeaders.size(); i++) {
+            String xpath = "//table[@id='table1']/tbody/tr[" + i + "]";
+            System.out.println(xpath);
+            WebElement singleRow = driver.findElement(By.xpath(xpath));
+            System.out.println(singleRow.getText());
+        }
+
+    }
+
+    @Test
+    public void getAllCellsInRow() {
+        List<WebElement> allCellsInOneRow = driver.findElements(By.xpath("//table[@id='table1']/tbody/tr[1]/td"));
+        for (WebElement cell : allCellsInOneRow) {
+            System.out.println(cell.getText());
+        }
+        System.out.println("number of cells in this row: " + allCellsInOneRow.size());
+    }
 
 
+    @Test
+    public void getSingeCellByIndex() {
+        WebElement singleCell = driver.findElement(By.xpath("//table[@id='table1']/tbody/tr[1]/td[4]"));
+        System.out.println(singleCell.getText());
+    }
+
+    @Test
+    public void printAllCellByIndex() {
+        // get number of rows
+        int rowNumber = getNumberOfRows();
+        // get number of columns
+        int colNumber = getNumberOfCols();
+    }
+
+    private int getNumberOfCols() {
+        List<WebElement> headers = driver.findElements(By.xpath("//table[@id='table1']//th"));
+        return headers.size();
+    }
+
+    /**
+     * returns number of row in the body of the table. skips the table header
+     *
+     * @return
+     */
+    private int getNumberOfRows() {
+        List<WebElement> allRowsExcludingHeaders = driver.findElements(By.xpath("//table[@id='table1']/tbody/tr"));
+        return allRowsExcludingHeaders.size();
+    }
 
 
 }
