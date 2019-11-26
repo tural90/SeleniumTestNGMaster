110ee77de65cf59fd1feeae60a3c1e5970d0bf33
CybertekSchool <github@cybertekschool.com>
2019-11-26 16:57:38 -0500

    fixed compilation error

--- src/test/java/com/cybertek/tests/d11_file_upload_actions_class/JavaScriptExecutorTest.java
+++ src/test/java/com/cybertek/tests/d11_file_upload_actions_class/JavaScriptExecutorTest.java
@@ -9,8 +9,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import javax.transaction.TransactionRequiredException;
-
 public class JavaScriptExecutorTest {
     WebDriver driver;
 
