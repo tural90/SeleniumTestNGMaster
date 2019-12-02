278b10e8fbce5fd008019a06985c70c25a9b254f
CybertekSchool <github@cybertekschool.com>
2019-12-02 15:25:06 -0500

    login page

--- src/test/java/com/cybertek/pages/LoginPage.java
+++ src/test/java/com/cybertek/pages/LoginPage.java
@@ -1,6 +1,8 @@
 package com.cybertek.pages;
 
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
@@ -8,4 +10,15 @@ public class LoginPage {
     public LoginPage(){
         PageFactory.initElements(Driver.get(), this);
     }
+
+    @FindBy(id="prependedInput")
+    public WebElement userName;
+
+    @FindBy(id="prependedInput2")
+    public WebElement password;
+
+    @FindBy(name = "_submit")
+    public WebElement submit;
+
+
 }
