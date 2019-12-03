abab5070260ce6d1c3b1c6ecd92c714969c30811
CybertekSchool <github@cybertekschool.com>
2019-12-03 14:54:26 -0500

    added more users

--- src/test/java/com/cybertek/tests/TestBase.java
+++ src/test/java/com/cybertek/tests/TestBase.java
@@ -3,6 +3,7 @@ package com.cybertek.tests;
 import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.Driver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -14,6 +15,7 @@ public class TestBase {
     protected WebDriver driver;
 
     protected  String url;
+    Actions actions;
 
     @BeforeMethod
     public void setupMethod() {
@@ -21,7 +23,7 @@ public class TestBase {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         url =  ConfigurationReader.get("url");
         driver.get(url);
-
+        actions = new Actions(driver);
     }
 
     @AfterMethod
