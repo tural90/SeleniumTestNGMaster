7923842605d57671cff125b9af57f5f5b5f3504a
CybertekSchool <github@cybertekschool.com>
2019-12-05 16:40:03 -0500

    another confclit

--- src/test/java/com/cybertek/pages/LoginPage.java
+++ src/test/java/com/cybertek/pages/LoginPage.java
@@ -22,8 +22,6 @@ public class LoginPage {
     public WebElement submit;
 
     public void login(String userNameStr, String passwordStr) {
-        userName.sendKeys(userNameStr);
-        password.sendKeys(passwordStr);
         submit.click();
     }
 
