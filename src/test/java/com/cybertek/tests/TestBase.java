cf26ac1cf1bfd21c727cd85c9f760e4b85ba29e7
CybertekSchool <github@cybertekschool.com>
2019-12-05 13:02:12 -0500

    extent p3

--- src/test/java/com/cybertek/tests/TestBase.java
+++ src/test/java/com/cybertek/tests/TestBase.java
@@ -3,12 +3,15 @@ package com.cybertek.tests;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.Driver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public abstract class TestBase {
@@ -53,7 +56,23 @@ public abstract class TestBase {
     }
 
     @AfterMethod
-    public void teardown() throws InterruptedException {
+    public void teardown(ITestResult result) throws InterruptedException, IOException {
+        // IF FAILED TAKE SCREENSHOT
+         if(result.getStatus() == ITestResult.FAILURE){
+             // record the name of the failed testcase
+             extentLogger.fail(result.getName());
+             // take screenshot and return location of the screenshot
+             String screenshot = BrowserUtils.getScreenshot(result.getName());
+             extentLogger.addScreenCaptureFromPath(screenshot);
+             // capture the exception
+             extentLogger.fail(result.getThrowable());
+         } else if (result.getStatus() == ITestResult.SKIP) {
+             // sometime tests are skipped, this is how we log skipped tests
+             extentLogger.skip("Test Skipped: " + result.getName());
+         }
+
+
+        // CLOSE BROWSER
         Thread.sleep(1000);
         Driver.closeDriver();
     }
--- src/test/java/com/cybertek/tests/d17_extent_reports/NegativeLoginTestWithReports.java
+++ src/test/java/com/cybertek/tests/d17_extent_reports/NegativeLoginTestWithReports.java
@@ -9,16 +9,34 @@ public class NegativeLoginTestWithReports  extends TestBase {
 
     @Test
     public void wrongPasswordTest(){
+        // if we dont add extentLogger = report.createTest ,this test will not be in the html report
         extentLogger = report.createTest("Wrong password test");
         LoginPage loginPage = new LoginPage();
-        extentLogger.info("Enter username");
+        extentLogger.info("Enter username: salesmanager101");
         loginPage.userName.sendKeys("salesmanager101");
-        extentLogger.info("Enter password");
+        extentLogger.info("Enter password: greatpassword");
         loginPage.password.sendKeys("greatpassword");
         extentLogger.info("Click submit");
         loginPage.submit.click();
         extentLogger.info("Verify page url");
         Assert.assertEquals(driver.getCurrentUrl(), "https://qa3.vytrack.com/user/login");
         extentLogger.pass("PASS: Wrong password test");
+    }
+
+    // this test will fail
+    @Test
+    public void wrongUsernameTest(){
+        // if we dont add extentLogger = report.createTest ,this test will not be in the html report
+        extentLogger = report.createTest("Wrong username test");
+        LoginPage loginPage = new LoginPage();
+        extentLogger.info("Enter username: salesmanager101");
+        loginPage.userName.sendKeys("salesmanager101");
+        extentLogger.info("Enter password: greatpassword");
+        loginPage.password.sendKeys("greatpassword");
+        extentLogger.info("Click submit");
+        loginPage.submit.click();
+        extentLogger.info("Verify page url");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://google.com/");
+        extentLogger.pass("PASS: Wrong username test");
     }
 }
