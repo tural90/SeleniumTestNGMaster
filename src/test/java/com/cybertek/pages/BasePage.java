9c5d0c173f4d535e66b5d70d0c0c69d558efad15
CybertekSchool <github@cybertekschool.com>
2019-12-10 17:05:32 -0500

    dataprovider 2

--- src/test/java/com/cybertek/pages/BasePage.java
+++ src/test/java/com/cybertek/pages/BasePage.java
@@ -18,10 +18,10 @@ public abstract class BasePage {
     protected WebElement loaderMask;
 
     @FindBy(css = "h1[class='oro-subtitle']")
-    protected WebElement pageSubTitle;
+    public WebElement pageSubTitle;
 
     @FindBy(css = "#user-menu > a")
-    protected WebElement userName;
+    public WebElement userName;
 
     @FindBy(linkText = "Logout")
     public WebElement logOutLink;
@@ -55,8 +55,9 @@ public abstract class BasePage {
             WebDriverWait wait = new WebDriverWait(Driver.get(), 5);
             wait.until(ExpectedConditions.invisibilityOf(loaderMask));
         } catch (Exception e) {
-            System.out.println("Loader mask doesn't present.");
+            e.printStackTrace();
         }
+
     }
 
     public String getUserName(){
